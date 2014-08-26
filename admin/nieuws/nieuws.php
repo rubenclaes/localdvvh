4d7c2672917816e13394dbd787e735b277f7e138
rubenclaes <rubes.claes@gmail.com>
2014-08-26 16:01:38 +0200

    all comited changes

--- admin/nieuws/nieuws.php
+++ admin/nieuws/nieuws.php
@@ -22,66 +22,6 @@ if (isset($_SESSION['userrole']) && ($_SESSION['userrole'] > 3)) {
 echo'</ul>';
 
 
-if ($fbuser) {
-  try {
-        //Get user pages details using Facebook Query Language (FQL)
-        $fql_query = 'SELECT page_id, name, page_url FROM page WHERE page_id IN (SELECT page_id FROM page_admin WHERE uid='.$fbuser.')';
-        $postResults = $facebook->api(array( 'method' => 'fql.query', 'query' => $fql_query ));
-    } catch (FacebookApiException $e) {
-        echo $e->getMessage();
-  }
-}else{
-        //Show login button for guest users
-        $loginUrl = $facebook->getLoginUrl(array('redirect_uri'=>$homeurl,'scope'=>$fbPermissions));
-        echo '<a href="'.$loginUrl.'"><img src="images/facebook-login.png" border="0"></a>';
-}
-if($fbuser && empty($postResults))
-{
-        /*
-        if user is logged in but FQL is not returning any pages, we need to make sure user does have a page
-        OR "manage_pages" permissions isn't granted yet by the user.
-        Let's give user an option to grant application permission again.
-        */
-        $loginUrl = $facebook->getLoginUrl(array('redirect_uri'=>$homeurl,'scope'=>$fbPermissions));
-        echo 'Could not get your page details, make sure you have created one!';
-        echo '<a href="'.$loginUrl.'">Click here to try again!</a>';
-}elseif($fbuser && !empty($postResults)){
-
-//Everything looks good, show message form.
-?>
-
-
-
-<div class="fbpagewrapper">
-<div id="fbpageform" class="pageform">
-<form id="form" name="form" method="post" action="process.php">
-<h1>Post to Facebook Page Wall</h1>
-<p>Choose a page to post!</p>
-<label>Pages
-<span class="small">Select a Page</span>
-</label>
-<select name="userpages" id="upages">
-    <?php
-    foreach ($postResults as $postResult) {
-            echo '<option value="'.$postResult["page_id"].'">'.$postResult["name"].'</option>';
-        }
-    ?>
-</select>
-<label>Message
-<span class="small">Write something to post!</span>
-</label>
-<textarea name="message"></textarea>
-<button type="submit" class="button" id="submit_button">Send Message</button>
-<div class="spacer"></div>
-</form>
-</div>
-</div>
-
-
-<?php
-}
-
- 
   if (isset($_SESSION['userrole']) && ($_SESSION['userrole'] >= 2)) {
     if (isset($_POST['adnieuws'])) {
         if (!empty($_POST['onderwerp']) && !empty($_POST['email']) && !empty($_POST['artikel'])) {
